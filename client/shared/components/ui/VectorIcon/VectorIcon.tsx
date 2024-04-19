c506d0cf6549dd082f0d6b5b166891e8e2e8d1cf
Anastasia Shviets <n.svheps@gmail.com>
2024-04-19 12:27:10 +0400

    chore: clean up

--- client/shared/components/ui/VectorIcon/VectorIcon.tsx
+++ client/shared/components/ui/VectorIcon/VectorIcon.tsx
@@ -2,15 +2,22 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { MD3Colors } from 'app/shared/providers/theme';
 import { ICON_NAMES } from 'app/shared/constants/iconNames';
 import { useTheme } from 'app/shared/hooks/styles/useTheme';
+import { ViewStyle } from 'react-native';
 type Iconprops = {
   iconName: keyof typeof ICON_NAMES;
-  size: number;
-  color: keyof MD3Colors;
+  size?: number;
+  color?: keyof MD3Colors;
+  style?: ViewStyle;
 };
 
-export const VectorIcon = ({ iconName, size, color }: Iconprops) => {
+export const VectorIcon = ({ iconName, size, color, style }: Iconprops) => {
   const theme = useTheme();
   return (
-    <Icon size={size} color={color ? theme.colors[color] : undefined} name={ICON_NAMES[iconName]} />
+    <Icon
+      size={size}
+      color={color ? theme.colors[color] : theme.colors.black0}
+      name={ICON_NAMES[iconName]}
+      style={style}
+    />
   );
 };
