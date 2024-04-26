838e18629688aa10092cfb96051136b7c345fbd1
Anastasia Shviets <n.svheps@gmail.com>
2024-04-26 12:05:11 +0400

    feat: Bottom Navigation Component

---
+++ client/features/goals/pages/Goals/GoalsPage.tsx
@@ -0,0 +1,5 @@
+import { Typography } from 'app/shared/components/ui';
+
+export const GoalsPage = () => {
+  return <Typography variant="h1">It's Goals Page</Typography>;
+};
---
+++ client/features/league/pages/League/LeaguePage.tsx
@@ -0,0 +1,4 @@
+import { Typography } from 'app/shared/components/ui';
+export const LeaguePage = () => {
+  return <Typography variant="h1">It's League Page</Typography>;
+};
