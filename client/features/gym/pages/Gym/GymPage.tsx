a220ac7755bbe3482da3a57f3dda80d7029d8603
Anastasia Shviets <n.svheps@gmail.com>
2024-04-26 12:05:11 +0400

    feat: Bottom Navigation Component

---
+++ client/features/gym/pages/Gym/GymPage.tsx
@@ -0,0 +1,4 @@
+import { Typography } from 'app/shared/components/ui';
+export const GymPage = () => {
+  return <Typography variant="h1">It's GYM Page</Typography>;
+};
---
+++ client/features/profile/pages/Profile/ProfilePage.tsx
@@ -0,0 +1,4 @@
+import { Typography } from 'app/shared/components/ui';
+export const ProfilePage = () => {
+  return <Typography variant="h1">It's Profile Page</Typography>;
+};
