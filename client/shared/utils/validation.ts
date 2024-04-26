d66d0dfe5462efc67099a906c2128c7316bc77a3
Anastasia Shviets <n.svheps@gmail.com>
2024-04-26 12:16:47 +0400

    feat: installing dependencies

--- client/shared/utils/validation.ts
+++ client/shared/utils/validation.ts
@@ -1,35 +1,59 @@
-import { z, ZodError } from 'zod';
-export const validateData = (schema: z.ZodObject<any, any>, data: any): Record<string, string> => {
-  try {
-    schema.parse(data);
-    return {};
-  } catch (error) {
-    if (error instanceof ZodError) {
-      const errors: Record<string, string> = {};
-      error.errors.forEach(err => {
-        const path = err.path.join('.');
-        errors[path] = err.message;
-      });
-      return errors;
-    } else {
-      throw error;
-    }
-  }
-};
+import isDate from 'validator/lib/isDate';
+import isEmail from 'validator/lib/isEmail';
+import isStrongPassword from 'validator/lib/isStrongPassword';
+import * as z from 'zod';
 
-export const PasswordSchema = z
+export const emailValidation = z.string().trim().email().refine(isEmail);
+
+export const passwordValidation = z
   .string()
-  .min(8, 'Password must contain at least 8 characters')
-  .regex(/[A-Z]/, 'Password must contain at least 1 uppercase letter')
-  .regex(/[0-9]/, 'Password must contain at least 1 number');
+  .min(6)
+  .refine(
+    isStrongPassword,
+    'Password must be 8+ chars, incl. uppercase, lowercase, number & special char.',
+  );
 
-export const EmailSchema = z.string().email('Invalid email format');
+const usernameRegex = /^(?!.*\.\.)(?!.*[.-]$)[^\W][\w.-]{0,29}$/;
+export const usernameValidation = z
+  .string()
+  .trim()
+  .refine(str => {
+    return str.match(usernameRegex);
+  }, 'Invalid username');
 
-export const UsernameSchema = z.string().min(3, 'Username must be at least 3 characters long');
+export const AgeSchema = z.coerce
+  .number({ message: 'Enter a number' })
+  .min(0, 'Enter a valid age')
+  .max(100);
 
-export const AgeSchema = z.number().min(0, 'Enter a valid age');
+const onlyLettersRegex = /^[a-zA-Z/-]+$/;
+export const firstNameValidationRequired = z
+  .string()
+  .trim()
+  .refine(str => {
+    return str?.match(onlyLettersRegex);
+  }, 'Invalid First Name');
+
+export const lastNameValidationRequired = z
+  .string()
+  .trim()
+  .refine(str => {
+    return str?.match(onlyLettersRegex);
+  }, 'Invalid Last Name');
+
+export const firstNameValidation = firstNameValidationRequired.optional();
+
+export const lastNameValidation = lastNameValidationRequired.optional();
+
+const BIRTHDAY_FORMAT = 'MM/dd/yyyy';
+export const birthdayValidation = z
+  .string()
+  .refine(str => isDate(str, BIRTHDAY_FORMAT), 'Invalid Date')
+  .optional();
 
 export const RegistrationShema = z.object({
-  email: EmailSchema,
-  password: PasswordSchema,
+  age: AgeSchema,
+  email: emailValidation,
+  password: passwordValidation,
+  name: usernameValidation,
 });
