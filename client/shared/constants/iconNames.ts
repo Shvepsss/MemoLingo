4b13c337ee447d376655658dd048d6fd7bc1e1bd
Shvepsss <120224092+Shvepsss@users.noreply.github.com>
2024-05-01 14:56:46 +0400

    Feat: Icon Component (#3)
    * feat: create Vector Icon Component
    
    * feat : Image Icon Component (SVG)
    
    ---------

---
+++ client/shared/constants/iconNames.ts
@@ -0,0 +1,18 @@
+export const ICON_NAMES = {
+  done: 'check',
+  star: 'star',
+  book: 'book',
+  close: 'close',
+  back: 'chevron-left',
+  more: 'chevron-down',
+  hide: 'chevron-up',
+  passwordHidden: 'eye-lock',
+  passwordShown: 'eye-lock-open',
+  shimmer: 'shimmer',
+  audio: 'volume-high',
+  flag: 'flag',
+  timer: 'timer-outline',
+  addFriend: 'account-plus-outline',
+  addedFriend: 'account-check-outline',
+  mistake: 'repeat-variant',
+};
