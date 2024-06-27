b45a30353c980afcacd4a0484f91acfed196eaab
Anastasia Shviets <n.svheps@gmail.com>
2024-06-27 17:01:15 +0400

    fix: review

--- client/shared/components/ui/MascotteMessage/MascotteMessage.tsx
+++ client/shared/components/ui/MascotteMessage/MascotteMessage.tsx
@@ -1,49 +1,44 @@
-import { IconLocal, Typography, View, AudioButtons } from 'app/shared/components/ui/index';
-import { useTheme } from 'app/shared/hooks/styles';
+import { IconLocal, Typography, View, AudioButtons } from 'app/shared/components/ui';
 import { getDynamicStylesInput, useDynamicStyles } from 'app/shared/hooks/styles/useDynamicStyles';
+import * as S from 'app/shared/styles/@style-atoms';
 
-import { MASCOTTE_ICONS, SIZE } from './constants';
+import { MASCOTTE_ICONS, MASCOTTE_TYPES, SIZE } from './constants';
 import { type MascotteMessageProps } from './types';
 
 export const MascotteMessage = ({ mascotteVariant, data, sizeVariant }: MascotteMessageProps) => {
   const icon = MASCOTTE_ICONS[mascotteVariant];
-  const theme = useTheme();
+  const MESSAGE_PADDING = mascotteVariant === MASCOTTE_TYPES.audio ? 0 : 15;
 
-  const dynamicStylesInput = getDynamicStylesInput(() => {
+  const dynamicStylesInput = getDynamicStylesInput(theme => {
     return {
       messageContainer: {
-        flexWrap: 'wrap',
-        flex: 1,
-        borderRadius: 10,
-        flexDirection: 'row',
-        borderWidth: 1,
-        justifyContent: 'center',
-        padding: mascotteVariant === 'audio' ? 0 : 15,
-        alignItems: 'center',
-        gap: 5,
+        padding: MESSAGE_PADDING,
         backgroundColor: theme.colors.black0,
         borderColor: theme.colors.primary60,
-      },
-      commonContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
       },
     };
   });
   const styles = useDynamicStyles(dynamicStylesInput);
 
   return (
-    <View style={styles.commonContainer}>
+    <View style={[S.flex.row, S.alignFlex.aCenter]}>
       <IconLocal icon={icon} size={SIZE[sizeVariant]} />
-      <View style={styles.messageContainer}>
-        {mascotteVariant === 'audio' ? (
+      <View
+        style={[
+          S.flex.row,
+          S.alignFlex.bothCenter,
+          S.flex.one,
+          S.flex.wrap,
+          S.gapAll.gx3,
+          S.border.wid1,
+          S.border.radx2Half,
+          styles.messageContainer,
+        ]}
+      >
+        {mascotteVariant === MASCOTTE_TYPES.audio ? (
           <AudioButtons url={data} />
         ) : (
-          <Typography
-            variant="smallRegular"
-            color="black100"
-            style={{ color: theme.colors.black100 }}
-          >
+          <Typography variant="smallRegular" color="black100">
             {data}
           </Typography>
         )}
--- client/shared/utils/data/mixArray.ts
+++ client/shared/utils/data/mixArray.ts
@@ -1,3 +1,3 @@
-export const mixArray = (array: any[]) => {
+export const mixArray = <T extends any[]>(array: T): T => {
   return array.sort(() => Math.random() - 0.5);
 };
